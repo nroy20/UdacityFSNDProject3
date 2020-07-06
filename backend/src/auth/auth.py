a85965c4d4d74b6490d0bb145bf044c1f8afa2ea
nroy20 <nroy20@illinois.edu>
2020-07-05 19:13:20 -0500

    Implemented get_token_auth_header() in auth.py within backend

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -31,8 +31,20 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    if 'Authorization' not in request.headers:
+        #replace w AuthError
+        abort(401)
 
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+
+    return header_parts[1]
+        
 '''
 @TODO implement check_permissions(permission, payload) method
     @INPUTS
